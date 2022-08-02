93105c1522997744ca3a59fca3a2de6feecbdce4
ycy <1647343313@qq.com>
2022-08-02 17:32:48 +0800

    init

---
+++ src/main/java/com/ycy/Jumpserver20191103224Application.java
@@ -0,0 +1,13 @@
+package com.ycy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jumpserver20191103224Application {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(Jumpserver20191103224Application.class, args);
+    }
+    
+}
---
+++ src/test/java/com/ycy/Jumpserver20191103224ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ycy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jumpserver20191103224ApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
