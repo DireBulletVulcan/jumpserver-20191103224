0d4ad836dd9dc1fdcf76d91aeebf7ff86ea29be5
ycy <1647343313@qq.com>
2022-08-05 20:34:51 +0800

    synchronizing code with teacher third

--- src/main/java/com/ycy/controller/HostController.java
+++ src/main/java/com/ycy/controller/HostController.java
@@ -1,15 +1,69 @@
 package com.ycy.controller;
 
+import com.ycy.model.Host;
+import com.ycy.repository.HostRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 @Controller
 @RequestMapping("host")
 public class HostController {
     
+    
+    // 标记了@Resource的属性，Springboot会自动注入
+    @Resource
+    HostRepository hostRepository;
+    
     @RequestMapping("list")
-    public String list() {
+    public String list(ModelMap map) {
+        List<Host> allHosts = this.hostRepository.findAll();
+        map.put("allHosts", allHosts);
         return "host/list";
+    }
+    
+    @GetMapping("save")
+    public String saveForm() {
+        return "host/save";
+    }
+    
+    // Springboot会将表单中的参数自动组装成Host对象
+    @PostMapping("save")
+    public String save(Host host) {
+        this.hostRepository.save(host);
+        return "redirect:/host/list";
+    }
+    
+    @GetMapping("del")
+    public String del(Long id) {
+        this.hostRepository.deleteById(id);
+        return "redirect:/host/list";
+    }
+    
+    @GetMapping("update")
+    public String updateForm(ModelMap map, @RequestParam("id") Long id) {
+        Host host = this.hostRepository.findById(id).get();
+        map.put("host", host);
+        return "host/update";
+    }
+    
+    @PostMapping("update")
+    public String update(Host host) {
+        // 更新的逻辑：先从数据库中查找到已保持的对象，再更加表单提交的值，去更新此对象，最后保存
+        Host savedHost = this.hostRepository.findById(host.getId()).get();
+        
+        savedHost.setHostName(host.getHostName());
+        savedHost.setIp(host.getIp());
+        savedHost.setUserName(host.getUserName());
+        
+        this.hostRepository.save(savedHost);
+        return "redirect:/host/list";
     }
     
 }
---
+++ src/main/java/com/ycy/repository/HostRepository.java
@@ -0,0 +1,10 @@
+package com.ycy.repository;
+
+import com.ycy.model.Host;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HostRepository extends JpaRepository<Host, Long> {
+    
+}
