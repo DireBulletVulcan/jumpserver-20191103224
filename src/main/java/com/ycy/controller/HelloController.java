a433959292bc20b7d6eaaef50cf740c74b3b6f81
ycy <1647343313@qq.com>
2022-08-05 16:13:17 +0800

    layui init

--- src/main/java/com/ycy/controller/HelloController.java
+++ src/main/java/com/ycy/controller/HelloController.java
@@ -1,14 +1,22 @@
 package com.ycy.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/hello")
 public class HelloController {
     
     public String fun() {
         return "Hello";
+    }
+    
+    @RequestMapping("/test")
+    public String test(){
+        
+        return "test";
     }
     
 }
